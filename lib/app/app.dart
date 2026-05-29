bdde62dc20bf1c4d568ff3b9a0a47fe84aacbbaa
L_vv_n <134967146+LevShv@users.noreply.github.com>
2026-05-29 19:46:15 +0300

    core app

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../features/map/presentation/screens/map_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navius',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MapScreen(),
+    );
+  }
+}
