a40299283ff97d34b5bf0ff1d0d9fdc27956f107
L_vv_n <134967146+LevShv@users.noreply.github.com>
2026-05-29 19:45:44 +0300

    rename

---
+++ lib/features/map/presentation/bloc/map_bloc.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'map_event.dart';
+import 'map_state.dart';
+import '../../domain/usecases/get_current_location.dart';
+
+class MapBloc extends Bloc<MapEvent, MapState> {
+    final GetCurrentLocation getCurrentLocation;
+
+    MapBloc({required this.getCurrentLocation}) : super(const MapState()) {
+        on<LoadLocation>(_onLoadLocation);
+        on<CenterOnUser>(_onCenterOnUser);
+    }
+
+    Future<void> _onLoadLocation(
+        LoadLocation event,
+        Emitter<MapState> emit,
+        ) async {
+            emit(state.copyWith(isLoading: true));
+            try {
+                final location = await getCurrentLocation();
+                emit(state.copyWith(
+                    currentLocation: location,
+                    isLoading: false,
+                    error: null,
+                ));
+            }
+            catch (e) {
+                emit(state.copyWith(
+                    isLoading: false,
+                    error: e.toString(),
+                ));
+            }
+    }
+    
+
+    void _onCenterOnUser(
+        CenterOnUser event,
+        Emitter<MapState> emit,
+    ) {
+        // TODO: Событие для центрирования, обработка в UI
+    }
+    
+}
